673b89a7c3dbfa206e581a48d63b81916de220a6
Maria Carolina <demycar24@gmail.com>
2024-10-25 20:47:37 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const lampada = document.getElementById("lampada");
+// estamos guardando na variavel lampada uma tag
+// documente acessa a tag pelo ID
+//console.log(lampada);
+
+const button = document.getElementById("btn");
+//console.log(button)
+
+let lampOn = false;
+//false ou true é um dado boleano
+
+//() => {}; arrow function
+button.addEventListener("click", () => {
+    lampOn = !lampOn;
+    lampada.src = lampOn
+      ? "https://github.com/mariaccarolina/TurmaFDV/blob/main/imagens/lampada.png?raw=true"
+      : "https://github.com/mariaccarolina/TurmaFDV/blob/main/imagens/lampada-com-mago.png?raw=true";
+})// mudar a imagem
+// operador ternario -condicional
